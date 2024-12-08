1183a903c4dea58b260f5566e88d4eb9edf6b2fa
ahmed <salaheldinahmed2024@gmail.com>
2024-12-08 16:03:00 +0200

    Part 04 Variable Declaration and Comments

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Part 04 Variable Declaration and Comments
+            // single line comment
+            /* multi  
+             * line
+             * comment
+             */
+            int Number = 10;
+            Console.WriteLine("the number is"+Number);
+            Console.WriteLine($"the number is{Number}");
+            Console.WriteLine("the number ={0} {1}",Number,"Ahmed");
+
+            #endregion
+
+
         }
     }
 }
