abacddea22af6703c28ba94f3ec47b89d5117129
ahmed <salaheldinahmed2024@gmail.com>
2024-12-08 18:44:48 +0200

    Q1.Write a program that allows the user to enter a number then print it.

---
+++ assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1.Write a program that allows the user to enter a number then print it.
+            Console.Write("Please enter a number ");
+            int number=int.Parse(Console.ReadLine());
+            Console.WriteLine($"your number is: {number} ");
+            #endregion
+        }
+    }
+}
