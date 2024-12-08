07b8b5989f0024f9c26aa33c6b4b396c6d8a99e2
ahmed <salaheldinahmed2024@gmail.com>
2024-12-08 18:55:21 +0200

    Q2.Write C# program that converts a string to an integer, but the string contains non-numeric characters. And mention what will happen

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -5,9 +5,19 @@
         static void Main(string[] args)
         {
             #region Q1.Write a program that allows the user to enter a number then print it.
-            Console.Write("Please enter a number ");
-            int number=int.Parse(Console.ReadLine());
-            Console.WriteLine($"your number is: {number} ");
+            //Console.Write("Please enter a number ");
+            //int number=int.Parse(Console.ReadLine());
+            //Console.WriteLine($"your number is: {number} ");
+            #endregion
+
+            #region Q2.Write C# program that converts a string to an integer, but the string contains non-numeric characters. And mention what will happen 
+            //Console.WriteLine("enter a string to convert to an integer:");
+            
+            //string input = Console.ReadLine();
+            //int.TryParse(input, out int result);
+            //Console.WriteLine($"Result: {result}");
+            ////if i put non numeric char such as hello  resault will be :0
+
             #endregion
         }
     }
